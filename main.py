692d9c9038ff9854d9f61318b47d3d198b612ad1
Macarena Bianchi <122046911+mbianchi2@users.noreply.github.com>
2023-01-18 19:38:42 +0000

    Terminé la función de Administrador

--- main.py
+++ main.py
@@ -30,12 +30,10 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.secret_key = 'esto-es-una-clave-muy-secreta'
 
 
-
 @app.route('/')
 def index():
   session['sesion'] = False  
   return render_template('index.html')
-
 
 @app.route('/admin', methods=["GET", "POST"])
 def admin():
@@ -46,15 +44,9 @@ def admin():
     return render_template('admin.html')
 
 
-@app.route('/opcionesAdmin', methods=["GET", "POST"])
-def opcionesAdmin():
-  return render_template('opcionesAdmin.html')
-
 @app.route('/ingreso', methods=["GET", "POST"])
 def ingreso():
-  print("llega a ingreso")
   if (request.method == "POST"):
-    print("pasa el post")
     if (request.form["usuario"] != "" or request.form["password"] != ""):
       session['sesion'] = True
       session['usuario'] = request.form["usuario"]
@@ -63,9 +55,8 @@ def ingreso():
       q = f"""SELECT * FROM Usuarios WHERE nombre = '{session['usuario']}' and contraseña = '{session['contraseña']}'"""
       resu = conn.execute(q)
       lista = resu.fetchall()
-      print(lista)
       if len(lista) != 0:
-        if lista[0][0] != session['usuario'] or lista[0][1] != session['contraseña']:
+        if lista[0][1] != session['usuario'] or lista[0][2] != session['contraseña']:
           mensaje = 'Los datos ingresados son incorrectos'
           print('Los datos ingresados son incorrectos')
           return render_template('admin.html', mensaje = mensaje)
@@ -74,7 +65,7 @@ def ingreso():
         return render_template('admin.html', mensaje = mensaje)
       conn.commit()      
       conn.close()
-      return render_template('opcionesAdmin.html')
+      return redirect('/opcionesAdmin')
     else:
       mensaje = "Por favor rellene todos los campos"
       return render_template('admin.html', mensaje = mensaje)
@@ -83,8 +74,64 @@ def ingreso():
     return render_template('admin.html')
 
 
+@app.route('/opcionesAdmin', methods=["GET", "POST"])
+def opcionesAdmin():
+  print("opciones Admin")
+  return render_template('opcionesAdmin.html')
 
 
+@app.route('/agregarProductos', methods=["GET", "POST"])
+def agregarProductos():
+  print("agregar productos")
+  if (request.method == "POST"):
+    if session['sesion'] == True:
+      nombre = request.form["nombre"]
+      nombre = nombre.capitalize()
+      categoria = request.form["categoria"]
+      informacion = request.form["informacion"]
+      file = request.files['imagen']
+      filename = secure_filename(file.filename)
+      file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+      print(file_path)
+      file.save(file_path)
+      print(request.form.get('linea'))
+      
+      if request.form.get('linea') == 'Seguridad':
+        linea = "Seguridad"
+      elif request.form.get('linea') == 'Nautica':
+        linea = "Nautica"
+      elif request.form.get('linea') == 'Industria':
+        linea = "Industria"
+      elif request.form.get('linea') == 'Dispositivos':
+        linea = "Dispositivos"
+      else:
+        mensaje = "¡Elija un checkbox!"
+        print(mensaje)
+        return render_template('agregarProductos.html',
+                               mensaje = mensaje)
+      
+      conn = sqlite3.connect('ginhsonElektronik.db')
+      q = f"""SELECT imagen FROM Productos WHERE imagen = '{file_path}'"""
+      resu = conn.execute(q)
 
+      if informacion == "":
+        descripcion = False
+      else:
+        descripcion = True
+      
+      if resu.fetchone():
+        mensaje2 = "Por favor renombre el archivo, el anterior ya existe."
+        return render_template('agregarProductos.html', mensaje2 = mensaje2)
+      else:  
+        r = f"""INSERT INTO Productos (nombre, linea, descripcion, imagen, informacion, categoria) VALUES ('{nombre}', '{linea}', '{descripcion}', '{file_path}', '{informacion}', '{categoria}');"""
+        conn.execute(r)
+        conn.commit()
+        conn.close()
+      
+      return render_template('agregarProductos.html')
+    else:
+      return redirect('/admin')
+  else:
+    return render_template('agregarProductos.html')
 
 app.run(host='0.0.0.0', port=81)
--- static/scripts/main.js
+++ static/scripts/main.js
@@ -38,3 +38,42 @@ function formularioAdmin() {
       console.log(error); 
   }, });
 }
+
+function detectarImagen() {
+  let cargadorImagenes = document.getElementById("selecArchivo");
+  if(cargadorImagenes.files.length > 0) {
+    //Tengo imagenes subidas
+    document.getElementById("selecArchivoLabel").innerHTML = "Imágen Adjunta";
+  }
+  else {
+    document.getElementById("selecArchivoLabel").innerHTML = "Adjunte una imagen";
+  }
+}
+
+function redirigirAdminAgregar() {
+  location.href = "/agregarProductos";
+}
+
+function redirigirAdminBorrar() {
+  location.href = "/borrarProductos";
+}
+
+function redirigirAdminModificar() {
+  location.href = "/modificarProductos";
+}
+
+function pagAnterior() {
+  location.href = "/opcionesAdmin";
+}
+
+function deseleccionar(check1, check2, check3) {
+  if (document.getElementById(check1).checked == true) {
+    document.getElementById(check1).checked = false
+  }
+  if (document.getElementById(check2).checked == true) {
+    document.getElementById(chec2).checked = false
+  }
+  if (document.getElementById(check3).checked == true) {
+    document.getElementById(check3).checked = false
+  }
+}
